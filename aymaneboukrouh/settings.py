8a316d1c0722cd419a3bf7a822049c51deacf687
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-10-17 11:10:02 +0100

    mv db.sqlite3 to data folder

--- aymaneboukrouh/settings.py
+++ aymaneboukrouh/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'aymaneboukrouh.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data/db.sqlite3',
     }
 }
 
