7010ba74ca389f750c955a53e6104a89212c42af
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-10-17 15:01:52 +0100

    add CSRF_TRUSTED_ORIGINS

--- aymaneboukrouh/settings.py
+++ aymaneboukrouh/settings.py
@@ -124,3 +124,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS=['https://www.aymaneboukrouh.com', 'http://aymaneboukrouh.com']
